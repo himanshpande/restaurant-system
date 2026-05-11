ce1659125af6c79ebf2bb7c9ba9e6f3868b3e3ec
Himanshu <ui.dev1@notenetic.com>
2026-05-11 23:58:28 +0530

    Setup frontend and backend architecture for restaurant management system

---
+++ backend/prisma/migrations/20260511181006_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `Table` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `tableNumber` INTEGER NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `status` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `Table_token_key`(`token`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
